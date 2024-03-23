f4856cb8b8b5a8ca480c877b967f1c6f625fd387
Gabriel <gabechipaya@gmail.com>
2024-03-23 17:39:33 +0200

    final project

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,10 +2,20 @@ import React, { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Budget = ({ selectedCurrency }) => {
-  const { budget } = useContext(AppContext);
+  const { budget, setBudget } = useContext(AppContext);
   const [newBudget, setNewBudget] = useState(budget);
   const handleBudgetChange = (event) => {
     setNewBudget(event.target.value);
+    if (newBudget > 2000) {
+      // Display error message for exceeding 2000
+      alert("Budget cannot exceed 2000");
+    } else if (newBudget < 1660) {
+      // Display error message for being lower than 1660
+      alert("Budget cannot be lower than 1660");
+    } else {
+      // Update budget if it meets criteria
+      setBudget(newBudget);
+    }
   };
   return (
     <div className='alert alert-secondary'>
